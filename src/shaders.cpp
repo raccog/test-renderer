4357732e2e37924c97898463325144b1ed4841ee
Ryan Cohen <rcohenprogramming@gmail.com>
2022-01-20 21:35:23 -0500

    Updated Shader class.

--- src/shaders.cpp
+++ src/shaders.cpp
@@ -67,3 +67,15 @@ void Shader::bind() const {
 void Shader::unbind() const {
 	glUseProgram(0);
 }
+
+void Shader::setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(m_id, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(m_id, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
+}
--- src/shaders.h
+++ src/shaders.h
@@ -20,6 +20,12 @@ public:
 		return m_id;
 	}
 
+	void setBool(const std::string &name, bool value) const;
+
+	void setInt(const std::string &name, int value) const;
+
+	void setFloat(const std::string &name, float value) const;
+
 	void unbind() const;
 };
 
